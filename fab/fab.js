bee0fd4989d909c49b1fdd8e8a84a87ddb497d9f
Ralph Thomas <ralpht@gmail.com>
2014-10-24 12:11:23 -0700

    Playing around with a "tactile" floating action button.
    I need to stage it better, and I want to do a real launch action as well as a few other bits and bobs, but this is the basic scheme. Also you should be able to navigate it by tapping as well as by dragging.

---
+++ fab/fab.js
@@ -0,0 +1,254 @@
+'use strict';
+// JavaScript implementation of a more physical Floating Action Button
+// Copyright 2014 (C) Ralph Thomas, ralpht@gmail.com
+
+(function() {
+
+var epsilon = 0.001;
+function almostEqual(a, b, epsilon) { return (a > (b - epsilon)) && (a < (b + epsilon)); }
+function almostZero(a, epsilon) { return almostEqual(a, 0, epsilon); }
+
+/***
+ * Simple Spring implementation -- this implements a damped spring using a symbolic integration
+ * of Hooke's law: F = -kx - cv. This solution is significantly more performant and less code than
+ * a numerical approach such as Facebook Rebound which uses RK4.
+ *
+ * This physics textbook explains the model:
+ *  http://www.stewartcalculus.com/data/CALCULUS%20Concepts%20and%20Contexts/upfiles/3c3-AppsOf2ndOrders_Stu.pdf
+ */
+function Spring(mass, springConstant, damping) {
+    this._m = mass;
+    this._k = springConstant;
+    this._c = damping;
+    this._solution = null;
+    this._endPosition = 0;
+    this._startTime = 0;
+}
+Spring.prototype._solve = function(initial, velocity) {
+    var c = this._c;
+    var m = this._m;
+    var k = this._k;
+    // Solve the quadratic equation; root = (-c +/- sqrt(c^2 - 4mk)) / 2m.
+    var cmk = c * c - 4 * m * k;
+    if (cmk >= 0) {
+        // The spring is overdamped or critically damped; no bounces.
+        // x = c1*e^(r1*t) + c2*e^(r2t)
+        // Need to find r1 and r2, the roots, then solve c1 and c2.
+        var r1 = (-c - Math.sqrt(cmk)) / (2 * m);
+        var r2 = (-c + Math.sqrt(cmk)) / (2 * m);
+        var c2 = (velocity - r1 * initial) / (r2 - r1);
+        var c1 = initial - c2;
+
+        return {
+            x: function(t) { return (c1 * Math.pow(Math.E, r1 * t) + c2 * Math.pow(Math.E, r2 * t)); },
+            dx: function(t) { return (c1 * r1 * Math.pow(Math.E, r1 * t) + c2 * r2 * Math.pow(Math.E, r2 * t)); }
+            };
+    } else {
+        // The spring is underdamped, it has imaginary roots.
+        // r = -(c / 2*m) +- w*i
+        // w = sqrt(4mk - c^2) / 2m
+        // x = (e^-(c/2m)t) * (c1 * cos(wt) + c2 * sin(wt))
+        var w = Math.sqrt(4*m*k - c*c) / (2 * m);
+        var r = -(c / 2*m);
+        var c1= initial;
+        var c2= (velocity - r * initial) / w;
+            
+        return {
+            x: function(t) { return Math.pow(Math.E, r * t) * (c1 * Math.cos(w * t) + c2 * Math.sin(w * t)); },
+            dx: function(t) {
+                var power =  Math.pow(Math.E, r * t);
+                var cos = Math.cos(w * t);
+                var sin = Math.sin(w * t);
+                return power * (c2 * w * cos - c1 * w * sin) + r * power * (c2 * sin + c1 * cos);
+            }
+        };
+    }
+}
+Spring.prototype.x = function(t) {
+    if (!t) t = (new Date()).getTime();
+    return this._solution ? this._endPosition + this._solution.x((t - this._startTime) / 1000.0) : 0;
+}
+Spring.prototype.dx = function(t) {
+    if (!t) t = (new Date()).getTime();
+    return this._solution ? this._solution.dx((t - this._startTime) / 1000.0) : 0;
+}
+Spring.prototype.setEnd = function(x, t) {
+    if (!t) t = (new Date()).getTime();
+    if (x == this._endPosition) return;
+    var velocity = 0;
+    var position = this._endPosition;
+    if (this._solution) {
+        velocity = this._solution.dx((t - this._startTime) / 1000.0);
+        position = this._solution.x((t - this._startTime) / 1000.0);
+        if (almostZero(velocity, epsilon)) velocity = 0;
+        if (almostZero(position, epsilon)) position = 0;
+        position += this._endPosition;
+    }
+    if (this._solution && almostZero(position - x, epsilon) && almostZero(velocity, epsilon))
+        return;
+    this._endPosition = x;
+    this._solution = this._solve(position - this._endPosition, velocity);
+    this._startTime = t;
+}
+Spring.prototype.snap = function(x) {
+    this._startTime = (new Date()).getTime();
+    this._endPosition = x;
+    this._solution = {
+        x: function() { return 0; },
+        dx: function() { return 0; }
+    };
+}
+
+/*
+ * Now the interesting bit, a FAB menu item. The menu item has a spring which moves it from its natural layout
+ * position to being against the current cursor position. The menu has a separate spring which moves menu items
+ * from the origin to thier natural layout position.
+ *
+ * The menu item doesn't actually position itself, the menu does this with transforms derived from the spring
+ * positions.
+ */
+function MenuItem(title, image) {
+    // Build the DOM.
+    this._container = document.createElement('div');
+    this._container.className = 'fab-menu-item';
+    this._label = document.createElement('div');
+    this._label.className = 'fab-label';
+    this._label.innerText = title;
+    this._container.appendChild(this._label);
+    this._icon = document.createElement('div');
+    this._icon.className = 'fab-icon';
+    this._icon.style.backgroundImage = 'url(' + image + ')';
+    this._container.appendChild(this._icon);
+    // We need a spring to tell us how far away we should be from the cursor.
+    this._spring = new Spring(1, 400, 30); // 400 / 30 is slightly underdamped, so there will be a slight overbounce.
+}
+MenuItem.prototype.element = function() { return this._container; }
+MenuItem.prototype.icon = function() { return this._icon; }
+MenuItem.prototype.setCursorIsClose = function(isCursorClose) { this._spring.setEnd(isCursorClose ? 1 : 0); }
+MenuItem.prototype.cursorAttraction = function() { return this._spring.x(); }
+
+/*
+ * This is the menu itself. It creates an item for the cursor, and then a bunch of items for each option.
+ * It lays them out in a vertical stack. It listens for touch events to open the stack and to move the
+ * cursor item around. The cursor is the main button.
+ */
+function FloatingActionButton(title, image, items) {
+    // The spring that opens the menu.
+    this._openSpring = new Spring(1, 400, 20);
+    this._cursorSpring = new Spring(1, 300, 20);
+    this._container = document.createElement('div');
+    this._container.className = 'fab-menu';
+
+    this._cursor = new MenuItem(title, image);
+    this._container.appendChild(this._cursor.element());
+
+    this._items = [];
+    for (var i = 0; i < items.length; i++) {
+        var mi = new MenuItem(items[i].title, items[i].image);
+        mi.element().style.pointerEvents = 'none';
+        this._items.push(mi);
+        this._container.appendChild(mi.element());
+    }
+
+
+    var isOpen = false;
+    var self = this;
+    /*
+    this._cursor.element().addEventListener('click', function() {
+        isOpen = !isOpen; self._openSpring.setEnd(isOpen ? 1 : 0); },
+        false);
+    */
+    var touchInfo = { trackingID: -1 };
+    this._cursor.element().addEventListener('touchstart', function(e) {
+        if (touchInfo.trackingID != -1) return;
+        e.preventDefault();
+        touchInfo.trackingID = e.changedTouches[0].identifier;
+        touchInfo.x = e.changedTouches[0].pageX;
+        touchInfo.y = e.changedTouches[0].pageY;
+        isOpen = true;
+        self._openSpring.setEnd(1);
+    }, false);
+    this._cursor.element().addEventListener('touchmove', function(e) {
+        if (touchInfo.trackingID == -1) return;
+        e.preventDefault();
+        var dy = 0;
+        var found = false;
+        for (var i = 0; i < e.changedTouches.length; i++) {
+            if (e.changedTouches[i].identifier == touchInfo.trackingID) {
+                dy = e.changedTouches[i].pageY - touchInfo.y;
+                found = true;
+                break;
+            }
+        }
+        if (!found) return;
+        self._updateCursor(dy, true);
+    }, false);
+    this._cursor.element().addEventListener('touchend', function(e) {
+        if (touchInfo.trackingID == -1) return;
+        e.preventDefault();
+        for (var i = 0; i < e.changedTouches.length; i++) {
+            if (e.changedTouches[i].identifier == touchInfo.trackingID) {
+                touchInfo.trackingID = -1;
+                self._updateCursor(0, false);
+                isOpen = false;
+                self._openSpring.setEnd(0);
+                return;
+            }
+        }
+    }, false);
+
+    this._cursorPosition = 0;
+
+    this._layout();
+}
+FloatingActionButton.prototype.element = function() { return this._container; }
+FloatingActionButton.prototype._layout = function() {
+    function clamp(x, min, max) { return (x < min ? min : (x > max ? max : x)); }
+    var openAmount = this._openSpring.x();
+    var cursorPosition = this._cursorPosition * this._cursorSpring.x();
+    
+    var y = 0;
+    for (var i = 0; i < this._items.length; i++) {
+        var item = this._items[i];
+        y -= 80;
+        var naturalPosition = y * openAmount;
+        var cursorAttraction = item.cursorAttraction();
+        // The actual position is somewhere between the natural position (which is the layout
+        var computedPosition = naturalPosition * (1 - cursorAttraction) + cursorPosition * cursorAttraction;
+
+        item.element().style.webkitTransform = 'translate3D(0, ' + computedPosition + 'px, 0)';
+        item.element().style.opacity = clamp(openAmount * 1.3 - 0.1, 0, 1);
+        item.icon().style.webkitTransform = 'scale(' + (1 + cursorAttraction * 0.2) + ') translateZ(0)';
+    }
+    this._cursor.icon().style.webkitTransform = 'translate3D(0, ' + cursorPosition + 'px, 0)';
+
+    requestAnimationFrame(this._layout.bind(this));
+}
+FloatingActionButton.prototype._updateCursor = function(position, isActive) {
+    if (!isActive) {
+        this._cursorSpring.setEnd(0);
+        return;
+    }
+    this._cursorPosition = position;
+    this._cursorSpring.snap(1);
+    // Which menu item are we closest to?
+    var selected = ~~Math.round(-position / 80);
+    for (var i = 0; i < this._items.length; i++) {
+        var item = this._items[i];
+        var selectionIndex = i + 1;
+        item.setCursorIsClose(selectionIndex == selected);
+    }
+}
+
+/*
+ * Actually create the menu.
+ */
+var testMenu = new FloatingActionButton(
+    'Compose', 'img/compose.png', 
+    [
+        { title: 'Paul Krugman', image: 'img/krugman.png' },
+        { title: 'Sophocles', image: 'img/greek.jpg' },
+        { title: 'Ralph Thomas', image: 'img/ralpht.jpg' },
+    ]);
+document.body.appendChild(testMenu.element());
+})();
