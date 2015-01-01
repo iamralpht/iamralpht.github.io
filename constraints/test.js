528471579df9a29c2e370e76c186af460074b52b
Ralph Thomas <ralpht@gmail.com>
2014-12-31 23:12:34 -0800

    constraints: start on a really complex motion UI, the chrome mobile tab switcher.

--- constraints/test.js
+++ constraints/test.js
@@ -232,3 +232,77 @@ function makePagerExample(parentElement) {
 }
 
 makePagerExample(document.getElementById('pager-example'));
+
+function makeClankExample(parentElement) {
+    var context = new MotionContext();
+    var solver = context.solver();
+
+    var updateFunction = context.update.bind(context);
+
+    // Clank looks a lot like the twitter panels in y, except that we have a manipulator
+    // for each panel...
+    var lastTab = null;
+
+    var MIN_GAP = 5;
+    var TAB_HEIGHT = 350;
+
+    var spacing = new c.Variable({ name: 'spacing' });
+
+    for (var i = 0; i < 3; i++) {
+        var p = new Box('Tab ' + (i+1));
+        // These boxes are our tabs. Give them y and bottom variables.
+        p.y = new c.Variable({ name: 'tab-' + i + '-y' });
+        p.bottom = new c.Variable({ name: 'tab-' + i + '-bottom' });
+        p.x = 10;
+        p.right = 300;
+        // Make the panel 250 wide.
+        solver.add(eq(p.bottom, c.plus(p.y, TAB_HEIGHT), required));
+
+        // Pin the first panel to 0, and add a motion constraint.
+        if (i == 0) {
+            solver.add(eq(p.y, 0, medium, 100));
+            context.addMotionConstraint(new MotionConstraint(p.y, '==', 0));
+        } else {
+            // Don't allow the tab to go past the end of the previous tab.
+            //solver.add(leq(p.y, lastTab.bottom, medium, 1000));
+
+            // Don't allow the tab to go before the previous tab.
+            //solver.add(geq(p.y, c.plus(lastTab.y, MIN_GAP), medium, 1000));
+
+            // Introduce a variable representing the gap between this panel and
+            // the previous one.
+            var gap = new c.Variable({name: 'gap-' + i});
+
+            // Gap is defined as the gap between this tab and the previous.
+            solver.add(eq(gap, c.minus(p.y, lastTab.y), strong));//medium));
+            solver.add(geq(gap, 0, strong));//medium));
+            solver.add(leq(gap, TAB_HEIGHT/2, strong));//medium));
+
+            // The gap is also the spacing plus a slack variable. The slack
+            // has a stay on it to make cassowary prefer to not move it around.
+            // The slack is so that multitouch can move tabs around independently,
+            // but it shouldn't move much on its own.
+            //var slack = new c.Variable({name: 'gap-slack-' + i});
+            //solver.add(eq(gap, c.plus(c.times(spacing, i/3), slack), strong));
+            //solver.add(mediumStay(slack));
+            //
+            solver.add(eq(gap, c.times(spacing, 1/3), medium));
+            solver.add(mediumStay(gap));
+            
+            // Our manipulator handles the gap.
+            var manip = new Manipulator(p.y, solver, updateFunction, p.element(), 'y');
+            context.addManipulator(manip);
+        }
+        context.addBox(p);
+        parentElement.appendChild(p.element());
+        lastTab = p;
+
+        // Every tab gets a manipulator (and at some point I'll add multitouch support to the
+        // manipulators).
+        //var manip = new Manipulator(p.y, solver, updateFunction, p.element(), 'y');
+        //context.addManipulator(manip);
+    }
+    updateFunction();
+}
+
+makeClankExample(document.getElementById('clank-example'));
