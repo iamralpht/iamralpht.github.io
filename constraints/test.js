1bf7a3792edf3a773bf4eb70a71c04d4592d9755
Ralph Thomas <ralpht@gmail.com>
2015-01-02 21:19:30 -0800

    constraints: add scaling example.
    Scaling is linear to touch, so this falls together easily. With a better gesture library I could include horizontal scrolling as well, and make it more like Facebook Paper.

--- constraints/test.js
+++ constraints/test.js
@@ -223,7 +223,8 @@ function makePagerExample(parentElement) {
             }
             return 0;
         }, 200);
-    // XXX: Hackery; the constraint should decide if it applies or not.
+    // A captive constraint means that the constraint is applied even if the current
+    // motion would take the variable out of violation.
     motionConstraint.captive = true;
     context.addMotionConstraint(motionConstraint);
 
@@ -232,4 +233,69 @@ function makePagerExample(parentElement) {
 }
 
 makePagerExample(document.getElementById('pager-example'));
+
+function makeScalingExample(parentElement) {
+    var context = new MotionContext();
+    var solver = context.solver();
+
+    var scale = new c.Variable({name: 'scale'});
+
+    var box = new Box('Drag Me Upwards');
+
+    box.x = new c.Variable({name: 'x'});
+    box.y = new c.Variable({name: 'y'});
+    box.bottom = new c.Variable({name: 'bottom'});
+    box.right = new c.Variable({name: 'right'});
+
+    var width = c.minus(box.right, box.x);
+    var height = c.minus(box.bottom, box.y);
+
+    // The width and height are related: we must keep the aspect ratio.
+    //
+    //   width = height * aspect
+    //
+    var aspect = 320 / 480;
+    solver.add(eq(width, c.times(height, aspect), required));
+
+    // The height is controlled by the scale.
+    //
+    //  height = scale * 480
+    //
+    solver.add(eq(height, c.times(scale, 480), medium));
+
+    // The height starts out at 200.
+    //
+    //  height = 200
+    //
+    //solver.add(eq(height, 200, weak));
+
+    // The bottom of the box is pinned to the bottom of the screen, like FB Paper.
+    //
+    //  bottom = 480
+    //
+    solver.add(eq(480, box.bottom, strong));
+
+    // The box is centered horizontally.
+    // 
+    //  centerX := x + width/2
+    //  centerX = 320/2
+    //
+    var centerX = c.plus(box.x, c.times(width, 0.5));
+    solver.add(eq(centerX, 320/2, medium));
+
+    // Make a variable for width so that we can create a motion constraint for it.
+    // Motion constraints should probably be able to understand expressions, too...
+    var widthV = new c.Variable({name: 'width'});
+    solver.add(eq(widthV, width, required));
+    solver.add(geq(width, 150, weak));
+
+    // Motion constraints on scale. We express the constraints on other variables
+    context.addMotionConstraint(new MotionConstraint(widthV, '>=', 150));
+    context.addMotionConstraint(new MotionConstraint(box.y, '>=', 0));
+
+    parentElement.appendChild(box.element());
+    context.addBox(box);
+    context.addManipulator(new Manipulator(box.y, solver, context.update.bind(context), parentElement, 'y'));
+}
+makeScalingExample(document.getElementById('scaling-example'));
 
