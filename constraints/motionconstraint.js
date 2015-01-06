4331667d10b73c625e249c728484e816e018b8bd
Ralph Thomas <ralpht@gmail.com>
2015-01-06 11:19:52 -0800

    constraints: add a range type; unused, might not solve any good problems.

--- constraints/motionconstraint.js
+++ constraints/motionconstraint.js
@@ -40,6 +40,48 @@ var mc = {
         var nearest = (startNearest + difference) * b;
 
         return nearest - a;
+    },
+    // A range, where by default every value is disallowed. You can add allowed ranges or values
+    // to the range. It's like a single span from a region.
+    range: function() {
+        function Range() {
+            this._keys = [];
+        }
+        Range.prototype.addAllowedRange = function(start, end) {
+            this._keys.push([start, end]);
+            this._sort();
+        }
+        Range.prototype.addAllowedValue = function(value) {
+            this.addAllowedRange(value, value);
+        }
+        Range.prototype._sort = function() {
+            this._keys.sort(function(a, b) { return a[0] - b[0]; });
+        }
+        Range.prototype.op = function(a, b, end) {
+            if (this._keys.length == 0) return 0;
+            // The value starts out invalid. Now we walk the keys and see if it's in a valid section.
+            var closest = 0;
+            for (closest = 0; closest < this._keys.length; closest++) {
+                if (this._keys[closest][0] == end) return 0;
+                if (this._keys[closest][1] == end) return 0;
+
+                // If we're inside this pair then we're out of violation.
+                if (this._keys[closest][0] <= end && this._keys[closest][1] >= end) return 0;
+
+                // If we're past this pair then keep going.
+                if (this._keys[closest][1] < end) continue;
+                // If this is the first one past our value then it's the "closest" for now
+                if (this._keys[closest][0] >= end) break;
+            }
+            // If we got here then we're in an invalid range and need to find our way back to the closest.
+            if (closest > this._keys.length - 1) closest = this._keys.length - 1;
+            var distA = this._keys[closest][0] - end;
+            var distB = this._keys[closest][1] - end;
+            var dist = Math.min(distA, distB);
+            return a - (dist + end);
+        }
+
+        return new Range();
     }
 };
 
@@ -59,7 +101,7 @@ function MotionConstraint(variable, op, value, options) {
         this.op = op;
     }
     if (!options) options = {};
-    this.overdragCoefficient = options.overdragCoefficient || 0.75;
+    this.overdragCoefficient = options.hasOwnProperty('overdragCoefficient') ? options.overdragCoefficient : 0.75;
     this.physicsModel = options.physicsModel;
     this.captive = options.captive || false;
 }
