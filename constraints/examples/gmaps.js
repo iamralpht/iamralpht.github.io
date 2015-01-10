eb8d6dceb49df2beb31ac178dadad71f1e0fcd21
Ralph Thomas <ralpht@gmail.com>
2015-01-10 11:47:49 -0800

    constraints: further example cleanup.

--- constraints/examples/gmaps.js
+++ constraints/examples/gmaps.js
@@ -1,34 +1,10 @@
 "use strict";
 
 
-// Helpers to make cassowary.js a bit clearer.
-var weak = c.Strength.weak;
-var medium = c.Strength.medium;
-var strong = c.Strength.strong;
-var required = c.Strength.required;
-
-var eq  = function(a1, a2, strength, w) {
-  return new c.Equation(a1, a2, strength || weak, w||0);
-};
-var neq = function(a1, a2, a3) { return new c.Inequality(a1, a2, a3); };
-var geq = function(a1, a2, str, w) { return new c.Inequality(a1, c.GEQ, a2, str, w); };
-var leq = function(a1, a2, str, w) { return new c.Inequality(a1, c.LEQ, a2, str, w); };
-
-var stay = function(v, strength, weight) {
-  return new c.StayConstraint(v, strength||weak, weight||0);
-};
-var weakStay =     function(v, w) { return stay(v, weak,     w||0); };
-var mediumStay =   function(v, w) { return stay(v, medium,   w||0); };
-var strongStay =   function(v, w) { return stay(v, strong,   w||0); };
-var requiredStay = function(v, w) { return stay(v, required, w||0); };
-
-
-// Get the first element by class name.
-function ec(className) {
-    return document.querySelector('.' + className);
-}
-
 function makeGoogleMapsExample() {
+    // Get the first element by class name.
+    function ec(className) { return document.querySelector('.' + className); }
+
     var height = 480;
     var parentElement = ec('container');
     var context = new MotionContext();
--- constraints/examples/simple.js
+++ constraints/examples/simple.js
@@ -1,27 +1,5 @@
 "use strict";
 
-
-// Helpers to make cassowary.js a bit clearer.
-var weak = c.Strength.weak;
-var medium = c.Strength.medium;
-var strong = c.Strength.strong;
-var required = c.Strength.required;
-
-var eq  = function(a1, a2, strength, w) {
-  return new c.Equation(a1, a2, strength || weak, w||0);
-};
-var neq = function(a1, a2, a3) { return new c.Inequality(a1, a2, a3); };
-var geq = function(a1, a2, str, w) { return new c.Inequality(a1, c.GEQ, a2, str, w); };
-var leq = function(a1, a2, str, w) { return new c.Inequality(a1, c.LEQ, a2, str, w); };
-
-var stay = function(v, strength, weight) {
-  return new c.StayConstraint(v, strength||weak, weight||0);
-};
-var weakStay =     function(v, w) { return stay(v, weak,     w||0); };
-var mediumStay =   function(v, w) { return stay(v, medium,   w||0); };
-var strongStay =   function(v, w) { return stay(v, strong,   w||0); };
-var requiredStay = function(v, w) { return stay(v, required, w||0); };
-
 function makeTwitterPanelsExample(parentElement) {
     var context = new MotionContext();
     var solver = context.solver();
